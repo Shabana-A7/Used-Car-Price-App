73d12324984caf9591091bd808f286f70c0dda58
Shabana-A7 <shabanaammar31@gmail.com>
2024-09-30 09:18:29 +0400

    Create pred_uprice.py

---
+++ pred_uprice.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import joblib
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
